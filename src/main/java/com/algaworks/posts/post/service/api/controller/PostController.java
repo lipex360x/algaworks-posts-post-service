fdb6fdee09d659550ffb05f8ab8255add5787580
Felipe Bambirra <lipex360@gmail.com>
2026-01-08 08:38:58 +0000

    http status updated

--- src/main/java/com/algaworks/posts/post/service/api/controller/PostController.java
+++ src/main/java/com/algaworks/posts/post/service/api/controller/PostController.java
@@ -14,11 +14,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -34,6 +36,7 @@ public class PostController {
   private final PostService postService;
 
   @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
   public PostSummaryOutput create(@RequestBody @Valid PostInput input) {
     Post post = postAssembler.toEntity(input);
     postService.create(post);
